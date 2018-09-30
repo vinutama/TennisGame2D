3c23500442a418172bb8c2333377bd04b875ec68
vinutama <muhammadkhevin@gmail.com>
2018-09-30 11:11:45 +0700

    add script.js

---
+++ script-canvas.js
@@ -0,0 +1,164 @@
+let canvas,
+    canvasContext,
+    ballHorizon = 50,
+    speedBallHorizon = 10,
+    ballVertical = 50,
+    speedBallVertical = 4,
+    paddlePlayerOne = 250,
+    paddleComputer = 250,
+    playerOneScore = 0,
+    computerScore = 0,
+    winScreen = false;
+
+const PADDLE_THICK = 10;
+const PADDLE_HEIGHT = 100;
+const WINNING_SCORE = 5;
+
+function mousePosition(position) {
+    let rect = canvas.getBoundingClientRect(),
+        root = document.documentElement,
+        mouseHorizon = position.clientX - rect.left - root.scrollLeft,
+        mouseVertical = position.clientY - rect.top - root.scrollTop;
+    return {
+        x: mouseHorizon,
+        y: mouseVertical
+    };
+}
+
+function mouseClick() {
+    if (winScreen) {
+        playerOneScore = 0;
+        computerScore = 0;
+        winScreen = false;
+    }
+}
+
+window.onload = function () {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    let fps = 30;
+    setInterval(function () {
+        ballMovement();
+        object();
+    }, 1000 / fps);
+
+    canvas.addEventListener('mousedown', mouseClick)
+
+    canvas.addEventListener('mousemove', function (position) {
+        let mousePos = mousePosition(position);
+        paddlePlayerOne = mousePos.y - (PADDLE_HEIGHT / 2);
+    });
+}
+
+function resetBall() {
+    if (computerScore >= WINNING_SCORE || playerOneScore >= WINNING_SCORE) {
+        winScreen = true;
+    }
+    speedBallHorizon = -speedBallHorizon;
+    ballHorizon = canvas.width / 2;
+    ballVertical = canvas.height / 2;
+}
+
+function AImovement() {
+    let paddleComputerCenter = paddleComputer + (PADDLE_HEIGHT / 2);
+    if (paddleComputerCenter < ballVertical - 35) {
+        paddleComputer += 5;
+    } else if (paddleComputerCenter > ballVertical + 35) {
+        paddleComputer -= 5;
+    }
+}
+
+function ballMovement() {
+    if (winScreen) {
+        return;
+    }
+    AImovement();
+    ballHorizon += speedBallHorizon;
+    ballVertical += speedBallVertical;
+    if (ballHorizon > canvas.width) {
+        if (ballVertical > paddleComputer && ballVertical < paddleComputer + PADDLE_HEIGHT) {
+            speedBallHorizon = -speedBallHorizon;
+
+            let hitHorizon = ballVertical - (paddleComputer + PADDLE_HEIGHT / 2);
+            speedBallVertical = hitHorizon * 0.35;
+        } else {
+            playerOneScore++;
+            resetBall();
+        }
+    }
+    if (ballHorizon < 0) {
+        if (ballVertical > paddlePlayerOne && ballVertical < paddlePlayerOne + PADDLE_HEIGHT) {
+            speedBallHorizon = -speedBallHorizon;
+
+            let hitHorizon = ballVertical - (paddlePlayerOne + PADDLE_HEIGHT / 2);
+            speedBallVertical = hitHorizon * 0.35;
+        } else {
+            computerScore++;
+            resetBall();
+        }
+    }
+    if (ballVertical > canvas.height) {
+        speedBallVertical = -speedBallVertical;
+    }
+    if (ballVertical < 0) {
+        speedBallVertical = -speedBallVertical;
+    }
+}
+
+function object() {
+    //Background screen
+    colorObject(0, 0, canvas.width, canvas.height, 'grey');
+
+    //SHOWING WIN OR LOSE SCREEN
+    if (winScreen) {
+        canvasContext.fillStyle = 'black';
+        if (computerScore >= WINNING_SCORE) {
+            canvasContext.fillText('YOU LOSE!', canvas.width / 2 - 1, 200);
+        } else if (playerOneScore >= WINNING_SCORE) {
+            canvasContext.fillText('YOU WON!', canvas.width / 2 - 1, 200);
+        }
+        canvasContext.fillText('CLICK TO START A NEW GAME', canvas.width / 2 - 50, 500, 400);
+        return;
+    }
+
+    //NET DRAW
+    netDrawer();
+
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillText('HIT 5 POINTS TO WIN A GAME!', canvas.width / 2 - 200, 100);
+
+    //left player paddle
+    colorObject(0, paddlePlayerOne, PADDLE_THICK, PADDLE_HEIGHT, 'black');
+
+    //computer paddle
+    colorObject(canvas.width - PADDLE_THICK, paddleComputer, PADDLE_THICK, PADDLE_HEIGHT, 'black');
+
+    //Ball drawer
+    colorBall(ballHorizon, ballVertical, 10, 'black');
+
+    //Score display
+    canvasContext.fillText(playerOneScore, 100, 100);
+    canvasContext.fillText(computerScore, canvas.width - 100, 100);
+
+}
+
+function netDrawer() {
+    //NET DRAWER
+    for (let i = 0; i < canvas.height; i += 40) {
+        colorObject((canvas.width / 2) - 1, i, 2, 20, 'black');
+    }
+}
+
+function colorBall(centerX, centerY, radius, drawColor) {
+    //SHAPE AND COLOR BALL
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
+}
+
+function colorObject(leftX, topY, width, height, drawColor) {
+    //BACKGROUND
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+}
